55a4d7c6bdb8ea6a5245d6647655c003b0c1bc7d
aussdawut <aussadawut.r@kkumail.com>
2025-09-28 07:31:11 +0700

    Fix script errors and missing resource references

--- Scripts/player_simple.gd
+++ Scripts/player_simple.gd
@@ -1,3 +1,29 @@
+extends CharacterBody3D
+
 # Onready Variables
-@onready var animation = get_node("Ch14_nonPBR2/AnimationPlayer")
-@onready var camera = get_node("Camera3D")
+@onready var animation = $Ch14_nonPBR2/AnimationPlayer
+@onready var camera = $Camera3D
+
+@export var move_speed : float = 6
+@export var jump_force : float = 5
+
+var gravity = 20.0
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_force
+	
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if direction:
+		velocity.x = direction.x * move_speed
+		velocity.z = direction.z * move_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, move_speed)
+		velocity.z = move_toward(velocity.z, 0, move_speed)
+	
+	move_and_slide()
